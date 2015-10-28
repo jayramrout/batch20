b0e50d736eac8c2c2674fa26be9c5e639b692b6c
Jayram <jayram.rout@hotmail.com>
2015-10-27 20:46:14 -0400

    My First Check in

---
+++ CoreJava/src/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World " + new java.util.Date());
+		System.out.print(" Hello " + args[0]);
+	}
+}
---
+++ CoreJava/src/SecondProgram.java
@@ -0,0 +1,9 @@
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		System.out.println("SecondProgram.main()"); //systrace
+		System.out.println(); // sysout -> ctrl+space
+		System.out.println(); // SOP
+	}
+} // jayram.h2kinfosys@gmail.com
