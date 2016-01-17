87dee368cc0c0d40893965c4b518eba2b8939bd4
Jayram <jayram.rout@hotmail.com>
2016-01-16 22:12:39 -0500

    Update

---
+++ employeeweb/src/jrout/tutorial/servlet/session/SessionServlet.java
@@ -0,0 +1,58 @@
+package jrout.tutorial.servlet.session;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SessionServlet
+ */
+@WebServlet("/SessionServlet")
+public class SessionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SessionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		HttpSession session = request.getSession();
+		
+		if(session.isNew()){
+			out.println("I am in new session");
+		}else{
+			out.println("I am in an existing session");
+			
+		}
+		out.println("Send request Again <a href=\"" + 
+				response.encodeURL("http://localhost:8081/employeeweb/SessionServlet") + 
+				"\">Send Request Again</a>.");
+		
+	}
+
+}
